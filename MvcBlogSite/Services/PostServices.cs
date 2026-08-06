25a963bd2a418f7e4f49dace2c6e60537733925b
Precious Oyebanjo <preciousoyebanjo@globusbank.com>
2026-08-06 14:41:51 +0100

    Implemented Single responsibility principle

---
+++ MvcBlogSite/Services/PostServices.cs
@@ -0,0 +1,17 @@
+﻿using MvcBlogSite.Models;
+using System.Text.Json;
+using System.IO;
+
+namespace MvcBlogSite.Services
+{
+    public class PostServices
+    {
+        public List<Posts> GetAllPosts()
+        {
+            string path = "Data/posts.json";
+            string json = File.ReadAllText(path);
+            List<Posts> posts = JsonSerializer.Deserialize<Posts>>(json);
+            return posts;
+        }
+    }
+}
