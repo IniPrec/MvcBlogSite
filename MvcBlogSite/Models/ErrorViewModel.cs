8457a9ca9e08752e5113f538d3b97de1b9903167
Precious Oyebanjo <preciousoyebanjo@globusbank.com>
2026-08-06 13:56:20 +0100

    Add project files.

---
+++ MvcBlogSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcBlogSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MvcBlogSite/Models/Posts.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcBlogSite.Models
+{
+    public class Posts
+    {
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? Author { get; set; }
+    }
+}
