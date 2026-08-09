c6a95be2aac1cc606d4bf263d5d6738e7f68a490
Precious Oyebanjo <preciousoyebanjo@globusbank.com>
2026-08-09 11:50:18 +0100

    Styling the web app

---
+++ MvcBlogSite/Controllers/PostsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcBlogSite.Services;
+using MvcBlogSite.Models;
+
+namespace MvcBlogSite.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly PostServices _postServices;
+
+        public PostsController(PostServices postServices)
+        {
+            _postServices = postServices;
+        }
+
+        public IActionResult Index()
+        {
+            List<Post> posts = _postServices.GetAllPosts();
+            return View(posts);
+        }
+
+        public IActionResult Details(Guid id)
+        {
+            List<Post> posts = _postServices.GetAllPosts();
+            Post post = posts.FirstOrDefault(p => p.Id == id); // LINQ. Searches the list for the first post whose ID matches the provided ID. If no match is found, it returns null.
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+    }
+}
--- MvcBlogSite/Models/Post.cs
+++ MvcBlogSite/Models/Post.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; }
         public string? Title { get; set; }
         public string? Content { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime Date { get; set; }
         public string? Author { get; set; }
     }
 }
